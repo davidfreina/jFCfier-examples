cd625714c44965cdc32f9641576b993c6b12742c
David Freina <david@freina.at>
2022-01-17 23:12:56 +0100

    updated main for better benchmarking

--- monte-carlo/src/main/java/Main.java
+++ monte-carlo/src/main/java/Main.java
@@ -2,13 +2,19 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
+    private static final int ITERATIONS = 5;
+
     public static void main(String[] args) {
         MonteCarlo monteCarlo = new MonteCarlo();
         for (int i = 100000; i <= 1000000000; i *= 10) {
-            long start = System.nanoTime();
-            monteCarlo.calculateForN(i);
+            long avgTime = 0;
+            for (int j = 0; j < ITERATIONS; j++) {
+                long start = System.nanoTime();
+                monteCarlo.calculateForN(i);                if (j > 0)
+                    avgTime += (System.nanoTime() - start);
+            }
             System.out.println(
-                    String.format("n: %s, t: %sms", i, TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - start)));
+                    String.format("n: %s, t: %sms", i, TimeUnit.NANOSECONDS.toMillis(avgTime / (ITERATIONS - 1))));
         }
     }
 }
